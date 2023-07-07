6dbac0fa6a1de4d4a776e4e7886d122e9b5aab8b
abhirawat623 <abhirawat623@gmail.com>
2023-07-07 16:20:17 +0530

    none

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const mainBtn = document.querySelector(".btn");
 const modalContainer = document.querySelector('.container');
 const closeBtn =document.querySelector(".close");
 const saveBtn =document.querySelector(".save");
+const darkBtn = document.querySelector(".dark-btn");
+const textContainer = document.querySelector(".modals-container");
+const darkBody =document.querySelector("body");
 
 mainBtn.addEventListener("click", ()=>{  //for button to hide
     modalContainer.classList.remove("hide")}); 
@@ -16,4 +19,18 @@ mainBtn.addEventListener("click", ()=>{  //for button to hide
 
     saveBtn.addEventListener("click", ()=>{
         alert("text saved !");
+    })
+
+    //for button to dark
+
+    darkBtn.addEventListener("click", ()=>{
+        if(darkBtn.innerText==="Dark Mode"){
+            darkBtn.innerText="Light Mode";
+           }
+           else{
+            darkBtn.innerText="Dark Mode";
+           }
+       textContainer.classList.toggle("dark");
+       darkBody.classList.toggle("dark");
+       
     })
